d68871ec6a257499ff517bdeae34821ecce3c2ce
Yuri Siman <yurisimannogueira@gmail.com>
2021-03-05 19:30:59 -0300

    Configurando o AutoMapper

--- src/CompleteApp.App/Startup.cs
+++ src/CompleteApp.App/Startup.cs
@@ -34,6 +34,8 @@ namespace CompleteApp.App
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
+            services.AddAutoMapper(typeof(Startup));
+
             services.AddControllersWithViews();
 
             services.AddScoped<MvcDbContext>();
