2c9a527d8f6286d30aa386d2bd59b723fabc9dd4
Yuri Siman <yurisimannogueira@gmail.com>
2021-03-05 14:02:07 -0300

    Mapeando as Entidades com FluentAPI

--- src/CompleteApp.Business/Models/Fornecedor.cs
+++ src/CompleteApp.Business/Models/Fornecedor.cs
@@ -8,6 +8,7 @@ namespace CompleteApp.Business.Models
         public string Nome { get; set; }
         public string Documento { get; set; }
         public TipoFornecedor TipoFornecedor { get; set; }
+        public Endereco Endereco { get; set; }
         public bool Ativo { get; set; }
         public IEnumerable<Produto> Produtos { get; set; }
     }
---
+++ src/CompleteApp.Data/Mappings/FornecedorMapping.cs
@@ -0,0 +1,33 @@
+﻿using CompleteApp.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CompleteApp.Data.Mappings
+{
+    public class FornecedorMapping : IEntityTypeConfiguration<Fornecedor>
+    {
+        public void Configure(EntityTypeBuilder<Fornecedor> builder)
+        {
+            builder.HasKey(f => f.Id);
+
+            builder.Property(f => f.Nome)
+                .IsRequired()
+                .HasColumnType("varchar(200)");
+
+            builder.Property(f => f.Documento)
+                .IsRequired()
+                .HasColumnType("varchar(14)");
+
+            // 1 : 1 => Fornecedor : Endereco
+            builder.HasOne(f => f.Endereco)
+                .WithOne(e => e.Fornecedor);
+
+            // 1 : N => Fornecedor : Produtos
+            builder.HasMany(f => f.Produtos)
+                .WithOne(p => p.Fornecedor)
+                .HasForeignKey(p => p.FornecedorId);
+
+            builder.ToTable("Fornecedores");
+        }
+    }
+}
