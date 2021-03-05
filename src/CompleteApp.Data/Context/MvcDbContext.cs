82c823262855cfbec177fd8ba9c65512dc4430ba
Yuri Siman <yurisimannogueira@gmail.com>
2021-03-05 09:40:55 -0300

    Criando classe MvcDbContext para o contexto de dados

---
+++ src/CompleteApp.Data/Context/MvcDbContext.cs
@@ -0,0 +1,31 @@
+﻿using CompleteApp.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompleteApp.Data.Context
+{
+    public class MvcDbContext : DbContext
+    {
+        public MvcDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MvcDbContext).Assembly);
+
+            // Desabilitando o Delete Cascade
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+    }
+}
