f79cb26c05c97771f5b750e268f64127bb88b784
Yuri Siman <yurisimannogueira@gmail.com>
2021-03-08 13:37:24 -0300

    Ajustando as Views Models

--- src/CompleteApp.App/ViewModels/ProdutoViewModel.cs
+++ src/CompleteApp.App/ViewModels/ProdutoViewModel.cs
@@ -1,10 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CompleteApp.App.ViewModels
 {
@@ -35,6 +32,8 @@ namespace CompleteApp.App.ViewModels
         [DisplayName("Ativo?")]
         public bool Ativo { get; set; }
 
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [DisplayName("Fornecedor")]
         public Guid FornecedorId { get; set; }
 
         public FornecedorViewModel Fornecedor { get; set; }
