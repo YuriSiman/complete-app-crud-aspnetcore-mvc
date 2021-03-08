435443465d9fc2f0a608ef7cf490337dbe95e7bc
Yuri Siman <yurisimannogueira@gmail.com>
2021-03-08 13:33:30 -0300

    Adicionando arquivos ignorados do git

---
+++ src/CompleteApp.App/AutoMapper/AutoMapperConfig.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using CompleteApp.App.ViewModels;
+using CompleteApp.Business.Models;
+
+namespace CompleteApp.App.AutoMapper
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<Fornecedor, FornecedorViewModel>().ReverseMap();
+            CreateMap<Endereco, EnderecoViewModel>().ReverseMap();
+            CreateMap<Produto, ProdutoViewModel>().ReverseMap();
+        }
+    }
+}
---
+++ src/CompleteApp.App/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CompleteApp.App.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
