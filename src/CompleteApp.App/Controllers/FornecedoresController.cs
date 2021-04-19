0142c4a8d12e669e2a8435f4c9c43e69443f4997
Yuri Siman <yurisimannogueira@gmail.com>
2021-04-19 13:55:08 -0300

    Implementando Modal Window para Atualizar o Endereço

--- src/CompleteApp.App/Controllers/FornecedoresController.cs
+++ src/CompleteApp.App/Controllers/FornecedoresController.cs
@@ -12,12 +12,14 @@ namespace CompleteApp.App.Controllers
     public class FornecedoresController : MainController
     {
         private readonly IFornecedorRepository _fornecedorRepository;
+        private readonly IEnderecoRepository _enderecoRepository;
         private readonly IMapper _mapper;
 
-        public FornecedoresController(IFornecedorRepository fornecedorRepository, IMapper mapper)
+        public FornecedoresController(IFornecedorRepository fornecedorRepository, IMapper mapper, IEnderecoRepository enderecoRepository)
         {
             _fornecedorRepository = fornecedorRepository;
             _mapper = mapper;
+            _enderecoRepository = enderecoRepository;
         }
 
         public async Task<IActionResult> Index()
@@ -96,6 +98,30 @@ namespace CompleteApp.App.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        public async Task<IActionResult> EditEndereco(Guid id)
+        {
+            var fornecedorViewModel = await ObterFornecedorEndereco(id);
+
+            if (fornecedorViewModel == null) return NotFound();
+
+            return PartialView("_EditEndereco", new FornecedorViewModel { Endereco = fornecedorViewModel.Endereco });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditEndereco(FornecedorViewModel fornecedorViewModel)
+        {
+            ModelState.Remove("Nome");
+            ModelState.Remove("Documento");
+
+            if (!ModelState.IsValid) return PartialView("_EditEndereco", fornecedorViewModel);
+
+            await _enderecoRepository.Atualizar(_mapper.Map<Endereco>(fornecedorViewModel.Endereco));
+
+            var url = Url.Action("ObterEndereco", "Fornecedores", new { id = fornecedorViewModel.Endereco.FornecedorId });
+            return Json(new { success = true, url });
+        }
+
         private async Task<FornecedorViewModel> ObterFornecedorEndereco(Guid id)
         {
             return _mapper.Map<FornecedorViewModel>(await _fornecedorRepository.ObterFornecedorEndereco(id));
@@ -104,6 +130,15 @@ namespace CompleteApp.App.Controllers
         private async Task<FornecedorViewModel> ObterFornecedorProdutosEndereco(Guid id)
         {
             return _mapper.Map<FornecedorViewModel>(await _fornecedorRepository.ObterFornecedorProdutosEndereco(id));
+        }
+
+        public async Task<IActionResult> ObterEndereco(Guid id)
+        {
+            var fornecedorViewModel = await ObterFornecedorEndereco(id);
+
+            if (fornecedorViewModel == null) return NotFound();
+
+            return PartialView("_IndexEndereco", fornecedorViewModel);
         }
     }
 }
--- src/CompleteApp.App/wwwroot/js/site.js
+++ src/CompleteApp.App/wwwroot/js/site.js
@@ -1,4 +1,42 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function AjaxModal() {
 
-// Write your JavaScript code.
+	$(document).ready(function () {
+		$(function () {
+			$.ajaxSetup({ cache: false });
+
+			$("a[data-modal]").on("click",
+				function (e) {
+					$('#myModalContent').load(this.href,
+						function () {
+							$('#myModal').modal({
+								keyboard: true
+							},
+								'show');
+							bindForm(this);
+						});
+					return false;
+				});
+		});
+
+		function bindForm(dialog) {
+			$('form', dialog).submit(function () {
+				$.ajax({
+					url: this.action,
+					type: this.method,
+					data: $(this).serialize(),
+					success: function (result) {
+						if (result.success) {
+							$('#myModal').modal('hide');
+							$('#EnderecoTarget').load(result.url); // Carrega o resultado HTML para a div demarcada
+							window.location.reload(false); // Recarregando a página
+						} else {
+							$('#myModalContent').html(result);
+							bindForm(dialog);
+						}
+					}
+				});
+				return false;
+			});
+		}
+	});
+}
