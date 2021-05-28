0211b3298c401333e1e3dbd66f1f6addcc232139
Yuri Siman <yurisimannogueira@gmail.com>
2021-05-28 18:52:38 -0300

    Adicionando arquivos não versionados pelo git

---
+++ src/CompleteApp.App/Extensions/IdentityAuthorize/CustomAuthorization.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Linq;
+
+namespace CompleteApp.App.Extensions.IdentityAuthorize
+{
+    public class CustomAuthorization
+    {
+        public static bool ValidarClaimsUsuario(HttpContext context, string claimName, string claimValue)
+        {
+            return context.User.Identity.IsAuthenticated && context.User.Claims.Any(c => c.Type == claimName && c.Value.Contains(claimValue));
+        }
+    }
+}
---
+++ src/CompleteApp.App/Extensions/IdentityAuthorize/RequisitoClaimFilter.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Routing;
+using System.Security.Claims;
+
+namespace CompleteApp.App.Extensions.IdentityAuthorize
+{
+    public class RequisitoClaimFilter : IAuthorizationFilter
+    {
+        private readonly Claim _claim;
+
+        public RequisitoClaimFilter(Claim claim)
+        {
+            _claim = claim;
+        }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            if (!context.HttpContext.User.Identity.IsAuthenticated)
+            {
+                context.Result = new RedirectToRouteResult(new RouteValueDictionary(new { area = "Identity", page = "/Account/Login", ReturnUrl = context.HttpContext.Request.Path.ToString() }));
+                return;
+            }
+
+            if(!CustomAuthorization.ValidarClaimsUsuario(context.HttpContext, _claim.Type, _claim.Value))
+            {
+                context.Result = new StatusCodeResult(403);
+            }
+        }
+    }
+}
